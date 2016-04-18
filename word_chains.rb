b237c215b0279ebed785c531d19af921c33d3120
LegaTux <josmpena91@gmail.com>
2016-04-18 21:29:35 +0200

    Upload Work to Github

--- word_chains.rb
+++ word_chains.rb
@@ -1,16 +1,31 @@
+require 'ruby-dictionary'
+require 'pry'
+
 class WordChain
-    def initialize(dictionary)
-        @dictionary = dictionary
+  def initialize(dictionary)
+    @dictionary = dictionary
   end
 
-        def find_chain
-            # Write your algorithm here
+  def find_chain(first = "cat", second = "dog")
+    # Write your algorithm here
+    output = first.dup
+    puts output
+    until output == second
+      second.chars.each_with_index do |char, index|
+        unless (output[index]) == char
+          output[index] = char
+          if @dictionary.exists?(output)
+            puts output if output != second
+          else
+            output[index] = first[index]
+          end
         end
+      end
+    end
+    puts output
   end
+end
 
-  my_chain = WordChain.new(???)
-  my_chain.find_chain("cat", "dog")
-  # cat
-  # cot
-  # cog
-  # dog
+dictionary = Dictionary.from_file("/usr/share/dict/words")
+my_chain = WordChain.new(dictionary)
+my_chain.find_chain("rice", "pine")
